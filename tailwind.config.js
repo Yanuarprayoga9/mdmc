29e6c2aa0b00e1b8ac6570e4b46e95f4299cf503
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-09-22 14:53:26 +0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1D4ED8',    // Custom primary color (blue)
+        secondary: '#F59E0B',  // Custom secondary color (yellow)
+        danger: '#EF4444',     // Custom danger color (red)
+        success: '#10B981',    // Custom success color (green)
+        semidark: '#323232',       // Custom dark color (grayish-black)
+        lightd: '#D9D9D9'
+      },
+    },
   },
   plugins: [],
 }
