faeb19d5909776faa82864583100e69987bfa4a8
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-09-23 17:11:36 +0700

    slider

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,10 @@
 function toggleNav() {
-    const navElement = document.querySelector('.navdrop');
-    
-    // Check if navElement contains 'hidden' class
-    if (navElement.classList.contains('hidden')) {
-        navElement.classList.remove('hidden'); // Remove 'hidden' class
-    } else {
-        navElement.classList.add('hidden'); // Add 'hidden' class
-    }
+  const navElement = document.querySelector(".navdrop");
+
+  // Check if navElement contains 'hidden' class
+  if (navElement.classList.contains("hidden")) {
+    navElement.classList.remove("hidden"); // Remove 'hidden' class
+  } else {
+    navElement.classList.add("hidden"); // Add 'hidden' class
+  }
 }
